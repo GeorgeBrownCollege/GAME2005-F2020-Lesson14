061790c8e164f819719a250accd832a3b38ed423
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-12-03 10:54:43 -0500

    initial commit

---
+++ Assets/_Scripts/CubeBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+
+        Gizmos.DrawWireCube(transform.position, new Vector3(1.0f, 1.0f, 1.0f));
+
+
+        Gizmos.color = new Color(1.0f, 1.0f, 0.0f, 1.0f);
+        Gizmos.DrawWireSphere(transform.position, 1.0f);
+    }
+}
